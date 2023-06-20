ceeb3f33c22832458e6587732e7a6a551368420f
Marco Viana <marcoviana100@gmail.com>
2023-06-20 17:11:00 -0300

    fix: correção da implementação de erros sob path_file

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -4,12 +4,12 @@ import os
 
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    file_extension = os.path.splitext(path_file)[1]
+    if file_extension != '.txt':
+        error_message = "Formato inválido"
+        sys.stderr.write(error_message + '\n')
+        sys.exit()
     try:
-        file_extension = os.path.splitext(path_file)[1]
-        if file_extension != '.txt':
-            error_message = "Formato inválido"
-            sys.stderr.write(error_message + '\n')
-            sys.exit()
 
         with open(path_file, 'r') as file:
             lines = file.readlines()
@@ -18,7 +18,8 @@ def txt_importer(path_file):
 
     except FileNotFoundError:
         error_message = f"Arquivo {path_file} não encontrado"
-        sys.stderr.write(error_message)
+        sys.stderr.write(error_message + '\n')
+        sys.exit()
 
 
 lines = txt_importer('statics/arquivo_teste.txt')
