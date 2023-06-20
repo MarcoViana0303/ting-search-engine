be16ff805fe53a76b114265cab2970b1bede8425
Marco Viana <marcoviana100@gmail.com>
2023-06-20 02:16:01 -0300

    feat: implementada a def txt_importer para importar noticias em arquivo txt, separado por /n

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,25 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    try:
+        file_extension = os.path.splitext(path_file)[1]
+        if file_extension != '.txt':
+            error_message = "Formato inválido"
+            sys.stderr.write(error_message)
+            return []
+
+        with open(path_file, 'r') as file:
+            lines = file.readlines()
+
+        return lines
+
+    except FileNotFoundError:
+        error_message = f"Arquivo {path_file} não encontrado"
+        sys.stderr.write(error_message)
+
+
+lines = txt_importer('statics/arquivo_teste.txt')
+print(lines)
