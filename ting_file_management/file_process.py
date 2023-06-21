bd993b7d84000ba51a5a23093a40357a92a0cc79
Marco Viana <marcoviana100@gmail.com>
2023-06-21 17:55:25 -0300

    feat: implementação da def process, responsável por gerar um dicionário

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+from file_management import txt_importer
+from queue import Queue
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    for item in instance.queue:
+        if item[""] == path_file:
+            return
+    lines = txt_importer(path_file)
+
+    if lines is not None:
+        file_data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines
+        }
+    instance.enqueue(file_data)
+    print(file_data)
 
 
 def remove(instance):
@@ -8,3 +25,7 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
+
+queue_instance = Queue()
+process('statics/arquivo_teste.txt', queue_instance)
