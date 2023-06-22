eddbfe474256892616e5084abff9e42ada981fef
Marco Viana <marcoviana100@gmail.com>
2023-06-22 03:37:02 -0300

    refactor: reimplementando a def txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,24 +1,16 @@
-from os.path import isfile
 import sys
-
-# if not path_file.endswith('.txt'):
-#     print('Formato inválido', file=sys.stderr)
-#     return
-# try:
-#     with open(path_file, 'r') as file:
-#         lines = file.read().splitlines()
-#         return lines
-
-# except FileNotFoundError:
-#     error_message = f"Arquivo {path_file} não encontrado"
-#     print(error_message, file=sys.stderr)
-#     retur
 
 
 def txt_importer(path_file):
-    if not isfile(path_file):
-        return print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
-    elif not path_file.endswith('.txt'):
-        return print('Formato inválido', file=sys.stderr)
-    doc = open(path_file)
-    return doc.read().split('\n')
+    if not path_file.endswith('.txt'):
+        print('Formato inválido', file=sys.stderr)
+        return
+    try:
+        with open(path_file, 'r') as file:
+            lines = file.read().splitlines()
+            return lines
+
+    except FileNotFoundError:
+        error_message = f"Arquivo {path_file} não encontrado"
+        print(error_message, file=sys.stderr)
+        return
