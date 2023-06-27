46361e4b1ba7120f30241b7171f59ba7f23a230b
Marco Viana <marcoviana100@gmail.com>
2023-06-27 18:36:56 -0300

    feat: implementação da def exists_word, responsável por contar palavras das queues

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,27 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    result = []
+
+    for file_data in instance._queue:
+        file_name = file_data["nome_do_arquivo"]
+        lines_with_word = []
+
+        for line_number, line in enumerate(
+            file_data["linhas_do_arquivo"],
+            start=1
+         ):
+            if word.lower() in line.lower():
+                lines_with_word.append({"linha": line_number})
+
+        if lines_with_word:
+            file_result = {
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": lines_with_word
+            }
+            result.append(file_result)
+
+    return result
 
 
 def search_by_word(word, instance):
