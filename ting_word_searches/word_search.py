69e0044ed30e56de3bd46954727bb5d37d617a8b
Marco Viana <marcoviana100@gmail.com>
2023-06-27 18:53:59 -0300

    feat: implementação da def search_by_word, responsável por mostrar as ocorrencias da palavra

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -26,3 +26,28 @@ def exists_word(word, instance):
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    result = []
+
+    for file_data in instance._queue:
+        file_name = file_data["nome_do_arquivo"]
+        occurrences = []
+
+        for line_number, line in enumerate(
+            file_data["linhas_do_arquivo"],
+            start=1
+         ):
+            if word.lower() in line.lower():
+                occurrence = {
+                    "linha": line_number,
+                    "conteudo": line.strip()
+                }
+                occurrences.append(occurrence)
+
+        if occurrences:
+            file_result = {
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": occurrences
+            }
+            result.append(file_result)
+    return result
